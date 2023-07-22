b81b9391663c466144d6151354eadef483559c86
Marcos Martins <122750376+Marcao-Martins@users.noreply.github.com>
2023-07-22 12:16:56 -0300

    Add files via upload

---
+++ CriterioOrdenacao.java
@@ -0,0 +1,17 @@
+public interface CriterioOrdenacao {
+
+    static CriterioOrdenacao fromString(String criterio) {
+        switch (criterio.toLowerCase()) {
+            case "descricao_c":
+                return new Descricao();
+            case "preco_c":
+                return new Preco();
+            case "estoque_c":
+                return new Estoque();
+            default:
+                throw new IllegalArgumentException("Criterio de ordenacao invalido!");
+        }
+    }
+
+    int comparar(Produto p1, Produto p2);
+}
